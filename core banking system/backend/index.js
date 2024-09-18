6e39451a331365723cb65049bc339c47c2813a17
Suraja Hasarinda <surajahasarinda@gmail.com>
2024-09-18 10:35:05 +0530

    Miner changes in the index.js file

--- core banking system/backend/index.js
+++ core banking system/backend/index.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import mysql from 'mysql';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 app.use(cors());
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: "root",
-    password: "2003",
-    database: "bank_database"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 })
 
 db.connect((err) => {
@@ -18,6 +20,12 @@ db.connect((err) => {
         return;
     }
     console.log('Connected to the database.');
+});
+
+const PORT = process.env.PORT || 8800; // 8800 is the default port
+
+app.listen(PORT, () => {
+    console.log(`Connected to backend!.....Server is running on http://localhost:${PORT}`);
 });
 
 app.get("/",(req,res)=>{
@@ -32,24 +40,27 @@ app.get("/accounts", (req, res) => {
     })
 })
 
-app.listen(8800, () => {
-  console.log('Connected to backend!.....Server is running on http://localhost:8800');
-});
+
 
 // get account summary
-app.get("/accounts_summary", (req, res) => {
-    const q = "SELECT * FROM bank_database.accounts_summary WHERE customer_id = 2"
-    db.query(q, (err, data)=>{
+app.get("/accounts_summary/:customerId", (req, res) => {
+    const customerId = req.params.customerId;
+    const q = "SELECT * FROM bank_database.accounts_summary WHERE customer_id = ?"
+
+    db.query(q, [customerId], (err, data)=>{
         if(err) return res.json(err)
         return res.json(data)
-    })
-})
+    });
+});
 
 // get recent transactions
-app.get("/recent_transactions", (req, res) => {
-    const q = "SELECT transaction_id, date, transaction_type, amount, description FROM bank_database.transaction_history WHERE customer_id = 1 LIMIT 3"
-    db.query(q, (err, data)=>{
+app.get("/recent_transactions/:customerId", (req, res) => {
+    const customerId = req.params.customerId;
+
+    const q = "SELECT transaction_id, date, transaction_type, amount, description FROM bank_database.transaction_history WHERE customer_id = ? LIMIT 3"
+
+    db.query(q, [customerId], (err, data)=>{
         if(err) return res.json(err)
         return res.json(data)
-    })
-})
+    });
+});
